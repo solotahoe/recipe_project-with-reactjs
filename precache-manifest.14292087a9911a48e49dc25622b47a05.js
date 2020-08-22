0dd8a210447e36ab7ff7d42f1025f8fe4098728d
solomon <githirasolomon@gmail.com>
2020-08-22 04:14:28 +0300

    Updates

---
+++ precache-manifest.14292087a9911a48e49dc25622b47a05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf550609cb4907a264c106cf3b8937ec",
+    "url": "/recipe_project-with-reactjs/index.html"
+  },
+  {
+    "revision": "45acb583bda1f1a63e8d",
+    "url": "/recipe_project-with-reactjs/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c5d9dbe1a179812fc6ca",
+    "url": "/recipe_project-with-reactjs/static/css/main.d8b0bb50.chunk.css"
+  },
+  {
+    "revision": "45acb583bda1f1a63e8d",
+    "url": "/recipe_project-with-reactjs/static/js/2.81450627.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe_project-with-reactjs/static/js/2.81450627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5d9dbe1a179812fc6ca",
+    "url": "/recipe_project-with-reactjs/static/js/main.07e8cb55.chunk.js"
+  },
+  {
+    "revision": "03a20b3c7be4bcdc2d9e",
+    "url": "/recipe_project-with-reactjs/static/js/runtime-main.27f75a91.js"
+  }
+]);
